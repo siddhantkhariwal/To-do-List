d3fea5cf670712e1528afd0823ac7749db1afe51
siddhantkhariwal <siddhant.209301640@muj.manipal.edu>
2022-12-30 04:45:04 +0530

    functionality to add new list items

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const https = require("https");
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set('view engine','ejs');
+
+var items = [];
 
 app.get("/",function(req,res){
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    var options = {
+        weekday :"long",
+        day : "numeric",
+        month : "long"
+    };
 
-    if(currentDay ==="6" || currentDay==="0"){
-        day = "Weekend";
-    }
-    else{
-        day = "Weekday";
-    }
-    res.render("list",{kindOfDay: day});
+    var day = today.toLocaleDateString("en-US", options);
+//the options parameter is passed in for passing options object to the date, and encoding it in english us(which is the locale thats why to locale date string, can write hi-IN for hindi as well).
+    res.render("list",{kindOfDay: day, newListItems:items});
 })
+
+app.post("/",function(req,res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
 
 
 
