2fcd09f68dd945758704241d7693da472ff1757b
siddhantkhariwal <siddhant.209301640@muj.manipal.edu>
2022-12-29 12:22:54 +0530

    first version

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const https = require("https");
+const app = express();
+
+app.set('view engine','ejs');
+
+app.get("/",function(req,res){
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    if(currentDay ==="6" || currentDay==="0"){
+        day = "Weekend";
+    }
+    else{
+        day = "Weekday";
+    }
+    res.render("list",{kindOfDay: day});
+})
+
+
+
+
+
+
+app.listen(3000,function(){
+    console.log("The server is running");
+})
