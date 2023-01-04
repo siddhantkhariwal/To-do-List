ff0b92c3873d9ac3f8979258a45f8d36dfa4689b
siddhantkhariwal <siddhant.209301640@muj.manipal.edu>
2023-01-04 08:27:29 +0530

    added footer and header in layout form

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.static("public"))
 app.set('view engine','ejs');
 
 var items = [];
+var workItems = [];
 
 app.get("/",function(req,res){
     var today = new Date();
@@ -19,18 +20,38 @@ app.get("/",function(req,res){
 
     var day = today.toLocaleDateString("en-US", options);
 //the options parameter is passed in for passing options object to the date, and encoding it in english us(which is the locale thats why to locale date string, can write hi-IN for hindi as well).
-    res.render("list",{kindOfDay: day, newListItems:items});
+    res.render("list",{listTitle : day, newListItems:items});
 })
 
+
+
 app.post("/",function(req,res){
+    console.log(req.body);
     var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    if(req.body.list ==="Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+    
 });
 
+app.get("/work",function(req,res){
+    res.render("list",{listTitle: "Work List", newListItems: workItems})
+})
 
+app.get("/about",function(req,res){
+    res.render("about");
+})
 
-
+// app.post("/work", function(req,res){
+//     var workItem = req.body.newItem;
+//     workItems.push(workItem);
+//     res.redirect("/work");
+// })
 
 
 app.listen(3000,function(){
